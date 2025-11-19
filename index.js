8c7182f9f80b2f362278d9fe2f3beb3d3d36be28
turtlewords <144561651+Turtlewords@users.noreply.github.com>
2025-11-19 11:15:14 -0600

    very beginning of markdown and initialize database

---
+++ index.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyC4Ud9qS09DAASqKIJNRJpoOF3ql_eJJBg",
+    authDomain: "note-locker-c8929.firebaseapp.com",
+    projectId: "note-locker-c8929",
+    storageBucket: "note-locker-c8929.firebasestorage.app",
+    messagingSenderId: "448490656040",
+    appId: "1:448490656040:web:67bd72e12336b08b5d001d"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
